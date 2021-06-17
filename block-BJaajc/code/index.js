ca7fef66d16cdcaabae17b7979d9088a00309b00
sohailshaik8328 <sohailshaik765@gmail.com>
2021-06-17 11:53:12 +0530

    block-2

---
+++ block-BJaajc/code/index.js
@@ -0,0 +1,31 @@
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
+
+function isPalindrome(value) {
+    for(let i = 0 ; i < value.length ; i++) {
+        for(let j = 0 ; j < value.length-1 ; j--) {
+            if(value[j] === value[i]) {
+                return true;
+            } else {
+                return false;
+            } 
+        }
+    }
+}
+
+function getCircumference(radius) {
+    return `The circumference is ${radius ** 2} NN.`;
+}
+
+function getArea(radius) {
+    return  `The area is ${radius * 2} NN.`;
+}
+
+
+module.exports = {
+    getFullName,
+    isPalindrome,
+    getCircumference,
+    getArea,
+}
---
+++ block-BJaajc/code/index.test.js
@@ -0,0 +1,17 @@
+const {getFullName, isPalindrome, getCircumference, getArea} = require('./index');
+
+test("getFullName is 'Arya' + 'Stark'", () => {
+    expect(getFullName("Arya", "Stark")).toBe("Arya Stark");
+});
+
+test("isPalindrome is madam", () => {
+    expect(isPalindrome("madam")).toEqual("madam");
+});
+
+test("getCircumference radius is 20 ", () => {
+    expect(getCircumference(20)).toBe(`The circumference is ${400} NN.`);
+});
+
+test("getArea radius is 30 ", () => {
+    expect(getArea(30)).toBe(`The area is ${60} NN.`);
+});
